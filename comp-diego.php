29762293f0035cd09dd2ed74f187fc9731ed0ab8
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-05-23 13:58:07 +0200

    Add hero-b

---
+++ comp-diego.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Diego
+*/
+?>
+<?php get_header() ?>
+
+<!-- Hero-b -->
+
+<?php include (locate_template('components/hero/hero-b.php', false, false)) ?>
+
+
+<?php get_footer() ?>
---
+++ components/hero/hero-b.php
@@ -0,0 +1,27 @@
+<section class="c--hero-b">
+    <div class="c--hero-b__hd">
+        <div class="c--hero-b__hd__wrapper">
+            <img
+                class="c--hero-b__hd__wrapper__icon"
+                src="<?=get_template_directory_uri() . "/assets/frontend/hero-b-icon.webp" ?>"
+                alt="Hero icon"
+            >
+            <hr class="c--hero-b__hd__wrapper__dash c--divider-a">
+            <h1 class="c--hero-b__hd__wrapper__title">Unlocking Acquisition Opportunities with Operator Insights.</h1>
+            <p class="c--hero-b__hd__wrapper__subtitle">Succession capital for owners who are ready for a partner in change</p>
+            <a href="#" class="c--hero-b__hd__wrapper__link">Contact Us</a>
+        </div>
+    </div>
+    <div class="c--hero-b__bd">
+        <img 
+            class="c--hero-b__bd__media c--hero-b__bd__media--is-desktop" 
+            src="<?=get_template_directory_uri() . "/assets/frontend/hero-b-media-desktop.webp" ?>" 
+            alt="Hero icon"
+        >
+        <img 
+            class="c--hero-b__bd__media c--hero-b__bd__media--is-mobile" 
+            src="<?=get_template_directory_uri() . "/assets/frontend/hero-b-media-mobile.webp" ?>" 
+            alt="Hero icon"
+        >
+    </div>
+</section>
