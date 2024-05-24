96dcea7c6c1e013bb2f1bac33d618da7d9f5fa54
Alberto Tocados <152868250+albertotocados@users.noreply.github.com>
2024-05-24 11:31:43 +0200

    navbar

--- src/js/modules/navbar/Navbar.js
+++ src/js/modules/navbar/Navbar.js
@@ -8,17 +8,26 @@ class Navbar {
             header: payload.header,
         };
 
+        this.isNavbarVisible = false; // Track the visibility state of the navbar
+
         this.init();
         this.events();
     }
 
     init() {
-        // Any initialization code can go here
+        // Set the initial position of the navbar to be offscreen right if in tablet range
+        if (this.isTabletScreen()) {
+            gsap.set(this.DOM.navbar, { x: '100%', autoAlpha: 0 });
+        }
     }
 
     events() {
         this.DOM.burger.addEventListener('click', () => {
             this.toggleNavbar();
+        });
+
+        window.addEventListener('resize', () => {
+            this.handleResize();
         });
     }
 
@@ -26,6 +35,48 @@ class Navbar {
         this.DOM.header.classList.toggle('c--header-a--is-active');
         this.DOM.burger.classList.toggle('c--burger-a--is-active');
         this.DOM.navbar.classList.toggle('c--nav-a__list-group--is-active');
+
+        if (this.isTabletScreen()) {
+            if (this.isNavbarVisible) {
+                // Animate navbar to offscreen right
+                gsap.to(this.DOM.navbar, {
+                    duration: 0.5,
+                    x: '100%',
+                    autoAlpha: 0,
+                    ease: 'power3.inOut',
+                });
+            } else {
+                // Make navbar visible before animation starts
+                gsap.to(this.DOM.navbar, {
+                    duration: 0.5,
+                    x: '0%',
+                    autoAlpha: 1,
+                    ease: 'power3.inOut'
+                });
+            }
+        }
+
+        this.isNavbarVisible = !this.isNavbarVisible; // Toggle the visibility state
+    }
+
+    handleResize() {
+        if (!this.isTabletScreen()) {
+            // Reset to pre-open state if the screen is not in tablet range
+            gsap.set(this.DOM.navbar, { x: '0%', autoAlpha: 1 });
+            this.DOM.header.classList.remove('c--header-a--is-active');
+            this.DOM.burger.classList.remove('c--burger-a--is-active');
+            this.DOM.navbar.classList.remove('c--nav-a__list-group--is-active');
+            this.isNavbarVisible = false;
+        } else {
+            // Ensure navbar is offscreen right if in tablet range
+            if (!this.isNavbarVisible) {
+                gsap.set(this.DOM.navbar, { x: '100%', autoAlpha: 0 });
+            }
+        }
+    }
+
+    isTabletScreen() {
+        return window.matchMedia('(max-width: 1024px)').matches;
     }
 }
 
