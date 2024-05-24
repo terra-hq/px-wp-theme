05c043ab6c424b4a0c401e79e4d566ddc3c09723
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-05-24 11:02:10 +0200

    Add content-a

--- comp-diego.php
+++ comp-diego.php
@@ -13,5 +13,17 @@ Template Name: Diego
 
 <?php include (locate_template('flexible-modules/highlighted-text-left-paragraph-right.php', false, false)); ?>
 
+<!-- Content -->
+<section class="f--section-b">
+    <div class="f--container">
+        <div class="f--row">
+            <div class="f--col-12">
+                <div class="c--content-a">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer() ?>
