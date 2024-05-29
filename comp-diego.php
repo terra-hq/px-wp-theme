7dfe0ca8a12cf691acb429e9b9cfa9599c230b4d
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-05-29 13:08:13 +0200

    Fix content paddings, margins and small mistakes

--- comp-diego.php
+++ comp-diego.php
@@ -3,7 +3,7 @@
 Template Name: Diego
 */
 ?>
-<?php get_header() ?>
+<?php get_header(); ?>
 
 <!-- Hero-b -->
 
@@ -22,7 +22,7 @@ Template Name: Diego
 <section class="f--section-b">
     <div class="f--container">
         <div class="f--row u--justify-content-center">
-            <div class="f--col-10 f--col-tabletm-12">
+            <div class="f--col-6 f--col-tabletm-8 f--col-tablets-10 f--col-mobile-12">
                 <div class="c--content-a">
                     <?php the_content(); ?>
                 </div>
