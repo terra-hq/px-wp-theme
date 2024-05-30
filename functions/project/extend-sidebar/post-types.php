33bb79e5016c6d9c11085324fcf6cedd0a75fd2f
guidogambini1995 <132886578+guidogambini1995@users.noreply.github.com>
2024-05-30 13:31:53 +0200

    blog

--- functions/project/extend-sidebar/post-types.php
+++ functions/project/extend-sidebar/post-types.php
@@ -57,7 +57,7 @@ function tf_thoughts_init()
         'rewrite' => array('slug' => 'thought', 'with_front' => false),
         'has_archive' => false,
         'show_in_rest' => true,
-        'supports' => array('title', 'thumbnail', 'editor', 'author'),
+        'supports' => array('title', 'thumbnail', 'editor'),
     );
     register_post_type('thought', $args);
 
