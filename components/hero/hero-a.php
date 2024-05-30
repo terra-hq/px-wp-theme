ca26d531da98f5d7900ee4e1fe01542deaf65198
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-05-30 11:08:01 +0200

    Change hero-a icon to svg format

--- components/hero/hero-a.php
+++ components/hero/hero-a.php
@@ -1,8 +1,15 @@
 <section class="c--hero-a">
     <div class="c--hero-a__hd">
         <div class="c--hero-a__hd__wrapper">
-            <img class="c--hero-a__hd__wrapper__icon"
-                src="<?= get_template_directory_uri() . "/assets/frontend/hero-b-icon.webp" ?>" alt="Hero icon">
+            <svg class="c--hero-a__hd__wrapper__icon" xmlns="http://www.w3.org/2000/svg"
+                width="42" height="32" viewBox="0 0 42 32" fill="none">
+                <path
+                    d="M0 31.9208V4.65405H40.9748V31.9208H34.1454V25.1039H6.82945V31.9208H0ZM34.1463 18.2879V11.471H6.82945V18.2879H34.1463Z"
+                    fill="#0A0A0A" />
+                <path d="M6.82945 0.0786133H0V4.65682H6.82945V0.0786133Z" fill="#0A0A0A" />
+                <path d="M23.6749 0.0786133H16.8455V4.65682H23.6749V0.0786133Z" fill="#0A0A0A" />
+                <path d="M40.9757 0.0786133H34.1462V4.65682H40.9757V0.0786133Z" fill="#0A0A0A" />
+            </svg>
             <hr class="c--hero-a__hd__wrapper__dash c--divider-a">
             <div class="c--hero-a__hd__wrapper__content">
 
