ca0fcbef9447005df50c605b046b37d35faede62
guidogambini1995 <132886578+guidogambini1995@users.noreply.github.com>
2024-05-30 11:39:00 +0200

    card d

--- components/card/card-d.php
+++ components/card/card-d.php
@@ -1,11 +1,7 @@
 <div class="c--card-d">
-    <?php
-    if ($index) { ?>
-        <div class="c--card-d__hd">
-            <div class="c--card-d__hd__badge"><?= $index + 1 ?></div>
-        </div>
-    <?php }
-    ?>
+    <div class="c--card-d__hd">
+        <div class="c--card-d__hd__badge"><?= $index + 1 ?></div>
+    </div>
     <div class="c--card-d__bd">
         <h3 class="c--card-d__bd__title"><?= $title ?></h3>
         <p class="c--card-d__bd__subtitle c--content-a">
