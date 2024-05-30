48ea0920af4970525581e0bd5040e9d98ddab2e1
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-05-30 17:26:55 +0200

    Update 404 image for a gif animation

--- 404.php
+++ 404.php
@@ -3,21 +3,15 @@
     <div class="g--404-01__wrapper">
         <div class="g--404-01__wrapper__content">
             <div class="g--404-01__wrapper__content__media-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" width="154" height="120" viewBox="0 0 154 120" fill="none">
-                    <g clip-path="url(#clip0_514_4203)">
-                        <path
-                            d="M0.0585938 119.703V17.4529H153.714V119.703H128.104V94.1398H25.669V119.703H0.0585938ZM128.107 68.5798V43.0163H25.669V68.5798H128.107Z"
-                            fill="#0A0A0A" />
-                        <path d="M25.669 0.294922H0.0585938V17.4632H25.669V0.294922Z" fill="#0A0A0A" />
-                        <path d="M88.8399 0.294922H63.2295V17.4632H88.8399V0.294922Z" fill="#0A0A0A" />
-                        <path d="M153.718 0.294922H128.107V17.4632H153.718V0.294922Z" fill="#0A0A0A" />
-                    </g>
-                    <defs>
-                        <clipPath id="clip0_514_4203">
-                            <rect width="153.882" height="120" fill="white" transform="translate(0.0585938)" />
-                        </clipPath>
-                    </defs>
-                </svg>
+                <img
+                    width="154" height="120"
+                    style="aspect-ratio: 154 / 120"
+                    decoding="async"
+                    src="<?php bloginfo('template_url'); ?>/assets/frontend/PX_404-animation.gif"
+                    data-src="<?php bloginfo('template_url'); ?>/assets/frontend/PX_404-animation.gif" 
+                    alt="404 image"
+                    class="g--lazy-01"
+                >
             </div>
             <h1 class="g--404-01__wrapper__content__item-primary">Page not found</h1>
             <p class="g--404-01__wrapper__content__item-secondary">The page you are looking for might have been removed,
