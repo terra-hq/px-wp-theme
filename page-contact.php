27adc904d60945c7b934b6292d47905785833056
guidogambini1995 <132886578+guidogambini1995@users.noreply.github.com>
2024-05-31 14:15:06 +0200

    contact

--- page-contact.php
+++ page-contact.php
@@ -6,4 +6,20 @@ Template Name: Contact
 
 <?php get_header() ?>
 
+<h2><?= get_field('title') ?></h2>
+<p><?= get_field('subtitle') ?></p>
+<?php
+$image_tag_args = array(
+    'image' => get_field('image'),
+    'class' => '',
+    'sizes' => ''
+);
+generate_image_tag($image_tag_args);
+?>
+<div class="c--form-a">
+    <div class="c--form-a__bd">
+        <?php echo do_shortcode('[contact-form-7 id="1ef0c76" title="Contact Form"]') ?>
+    </div>
+</div>
+
 <?php get_footer() ?>
