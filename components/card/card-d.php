47581a87a91d8c33b8ed60efd225e3551e40d6a2
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-05-31 13:56:19 +0200

    Fix content tag in card-d

--- components/card/card-d.php
+++ components/card/card-d.php
@@ -4,8 +4,8 @@
     </div>
     <div class="c--card-d__bd">
         <h3 class="c--card-d__bd__title"><?= $title ?></h3>
-        <p class="c--card-d__bd__subtitle c--content-a">
+        <div class="c--card-d__bd__subtitle c--content-a">
             <?= $subtitle ?>
-        </p>
+        </div>
     </div>
 </div>
