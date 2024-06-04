59e807ac5205c95dab7c0e24b246b05c279e3116
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2024-06-04 10:27:04 +0200

    Update hero-d font class in title from a to b

--- components/hero/hero-d.php
+++ components/hero/hero-d.php
@@ -2,7 +2,7 @@
     <div class="f--container">
         <div class="f--row u--justify-content-center">
             <div class="f--col-6 f--col-tabletm-8 f--col-tablets-10 f--col-mobile-12">
-                <h1 class="f--font-a f--mb-4">
+                <h1 class="f--font-b f--mb-4">
                     <?= $title ?>
                 </h1>
                 <p class="f--font-f u--font-medium f--mb-4">
